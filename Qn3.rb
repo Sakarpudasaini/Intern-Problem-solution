22a567c5fb1b77ae3b8df809c8a7336fc11e3ba5
Sakar <sakarpudasaini5@gmail.com>
2019-04-20 10:19:01 +0545

    Create Qn3.rb

---
+++ Qn3.rb
@@ -0,0 +1,12 @@
+def letter_count(str)
+  counts = {}
+
+  str.each_char do |char|
+    next if char == " "
+    counts[char] = 0 unless counts.include?(char)
+    counts[char] += 1
+  end
+
+  counts
+end
+puts letter_count('hello how are you')
